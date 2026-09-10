c3d4738c13e91ba04e3fd101e7f91d9b35459b91
kimgud486 <kimgud486@gmail.com>
2026-09-11 00:50:41 +0900

    feat(scanner): expose historical scan enrichment

--- explainable_scanner.py
+++ explainable_scanner.py
@@ -7,6 +7,14 @@ from scanner import (
     SignalValidationGate,
     detect_pattern
 )
+from scanner_history import (
+    PatternHistoryStats,
+    ScannerEnrichment,
+    calculate_dynamic_prices,
+    calculate_pattern_history,
+    build_simple_explanation,
+    enrich_scan_result,
+)
 
 __all__ = [
     "TradeIdea",
@@ -14,5 +22,11 @@ __all__ = [
     "scan_market",
     "print_explainable_top5",
     "SignalValidationGate",
-    "detect_pattern"
+    "detect_pattern",
+    "PatternHistoryStats",
+    "ScannerEnrichment",
+    "calculate_dynamic_prices",
+    "calculate_pattern_history",
+    "build_simple_explanation",
+    "enrich_scan_result",
 ]
