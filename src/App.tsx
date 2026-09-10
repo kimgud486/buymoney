f5d8ae501c4ba20c5683d24260b56d7f43bcfc8d
kimgud486 <kimgud486@gmail.com>
2026-09-11 00:11:59 +0900

    feat: mount operations status monitor on main dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { RealtimeMarketStreamManager } from "./components/RealtimeMarketStreamMa
 import { MultiModelSecuritiesConsensusModal } from "./components/MultiModelSecuritiesConsensusModal";
 import { MasterAiAutoTradingDashboard } from "./components/trading/MasterAiAutoTradingDashboard";
 import RealtimeHubStatusStrip from "./components/trading/RealtimeHubStatusStrip";
+import OperationsStatusMonitor from "./components/trading/OperationsStatusMonitor";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { MainScreenKoreanText } from "./components/MainScreenKoreanText";
 
@@ -50,6 +51,10 @@ function MainLayout() {
 
       <ErrorBoundary>
         <RealtimeHubStatusStrip />
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <OperationsStatusMonitor />
       </ErrorBoundary>
 
       <ErrorBoundary>
