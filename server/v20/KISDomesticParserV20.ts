58c72e743ce3a9cb30a2deb25986723762c56613
kimgud486 <kimgud486@gmail.com>
2026-09-10 11:33:35 +0900

    chore(v20): preserve migrated domestic parser compatibility

---
+++ server/v20/KISDomesticParserV20.ts
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+// AISTOCK V20 KIS DOMESTIC PARSER COMPATIBILITY ADAPTER
+// Migrated from jusik2 without duplicating the stronger buymoney parser.
+// ----------------------------------------------------------------------
+
+export type { DomesticTradeTickV20 } from "./KISDomesticTradeParserV20";
+export { KISDomesticTradeParserV20 as KISDomesticParserV20 } from "./KISDomesticTradeParserV20";
